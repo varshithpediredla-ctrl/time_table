80f9e1273806052e0cc6a5788aa70726629d1836
Varshith <varshithpediredla@gmail.com>
2026-07-13 20:50:57 +0530

    comment out missing Sidebar import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Sidebar from "./components/Sidebar";
+// import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 import FacultyView from "./components/FacultyView";
 import DashboardView from "./components/DashboardView";
